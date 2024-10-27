3af3c0f53218e0ed02e9859b05ebec47c5229908
Soumyadeep Majumdar <soum@MacBook-Air.local>
2024-10-27 00:36:23 -0700

    Part B is done.

--- course_project/setup_cron.sh
+++ course_project/setup_cron.sh
@@ -8,3 +8,16 @@ echo "Script path is: $script_path"
   (crontab -l 2>/dev/null; echo "0 0 * * * python3 $script_path") | crontab -
   echo "Cron job added to run log_rotation.py daily at midnight."
 }
+
+service_file="/etc/systemd/system/log_rotation.service"
+echo "[Unit]
+Description=Log Rotation Service
+
+[Service]
+ExecStart=$(which python3) $(pwd)/log_rotation.py
+
+[Install]
+WantedBy=multi-user.target" | sudo tee $service_file
+
+sudo systemctl enable log_rotation.service
+sudo systemctl start log_rotation.service
